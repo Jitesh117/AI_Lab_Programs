ae933fe6e1d3a25bc2ffd78b7da79d82ca8c0e65
Jitesh117 <sahookumarjitesh@gmail.com>
2023-12-10 22:39:32 +0530

    expert

--- crossword.lisp
+++ crossword.lisp
@@ -1,74 +1,60 @@
-defparameter *crossword-grid* 
-  '((#\- #\- #\- #\B #\Y #\T #\E)
-    (#\- #\- #\- #\R #\- #\- #\-)
-    (#\E #\N #\G #\I #\N #\E #\-)
-    (#\- #\- #\- #\C #\- #\W #\D)
-    (#\- #\G #\- #\K #\- #\I #\A)
-    (#\C #\P #\U #\- #\- #\F #\T)
-    (#\- #\U #\U #\S #\B #\I #\A)))
+(defparameter *crossword-grid* 
+ '((#\- #\G #\- #\H #\T #\M #\L)
+ (#\- #\I #\- #\A #\- #\- #\I)
+ (#\S #\T #\A #\C #\K #\- #\S)
+ (#\M #\- #\- #\K #\- #\- #\P)
+ (#\T #\- #\A #\- #\- #\- #\-)
+ (#\P #\O #\P #\- #\- #\- #\-)
+ (#\- #\- #\I #\D #\E #\- #\-)))
 (defparameter *clues*
-
-
-
-
-  '((:across 1 "It powers vehicles and machines with fuel or electricity.(6)" ENGINE)
-    (:across 2 " The brain of the computer(3)" CPU)
-    (:across 3 "Basic data storage unit(4)" BYTE)
-    (:across 4 "The component which connects peripheral(3)" USB)
-    (:down 5 "It's a rectangular building material made of clay or concrete, often used in construction.(5)" BRICK)
-    (:down 6 "Raw form of Information(4)" DATA)
-    (:down 7 "Graphical device which handles graphics(3)" GPU)
-    (:down 8 "Connecting Wirelessly(4)" WIFI)))
-
-
+ '((:across 1 "A data structure with LIFO behavior(5)" STACK)
+ (:across 2 "A protocol for sending electronic mail(3)" POP)
+ (:across 3 "A fundamental language for web development(4)" HTML)
+ (:across 4 "A tool for software development(3)" IDE)
+ (:down 5 "A common version control system(3)" GIT)
+ (:down 6 "To gain unauthorized access to a computer system(4)" HACK)
+ (:down 7 "A set of rules that allows softwares to communicate with each other(3)" API)
+ (:down 8 "A Symbolic language with list processing(4)" LISP)))
 (defun print-crossword-grid (grid)
-  (format t "~%CROSSWORD PUZZLE: ~%" )
-  (format t "(* indicate the cells where the words occur) ~%~%" )
-  (loop for row in grid
-    do (loop for cell in row
-      do (if (char= cell #\-)
-             (format t "~a " cell)
-             (format t "* ")))
-    do (format t "~%")))
-
+ (format t "~%CROSSWORD PUZZLE: ~%" )
+ (format t "(* indicate the cells where the words occur) ~%~%" )
+ (loop for row in grid
+ do (loop for cell in row
+ do (if (char= cell #\-)
+ (format t "~a " cell)
+ (format t "* ")))
+ do (format t "~%")))
 (defun print-answer (grid)
-    (loop for row in grid
-    do (loop for cell in row
-             do (format t "~a " cell))
-    do (format t "~%")))
-
+ (loop for row in grid
+ do (loop for cell in row
+ do (format t "~a " cell))
+ do (format t "~%")))
 (defun check-answer (user-input answer)
-      (if (string= user-input answer)
-      (format t "RIGHT!~%")
-      (progn
-        (format t "WRONG. Try again.~%")
-        (loop
-          (format t "Enter your answer again: ")
-          (finish-output)
-          (let ((user-input (read-line *query-io*)))
-            (if (string= user-input answer)
-
-
-
-                (progn
-                  (format t "RIGHT!~%")
-                  (return)) ; Exit the loop if the answer is correct
-                (format t "WRONG. Try again.~%")))))))
-
-
+ (if (string= user-input answer)
+ (format t "RIGHT!~%")
+ (progn
+ (format t "WRONG. Try again.~%")
+ (loop
+ (format t "Enter your answer again: ")
+ (finish-output)
+ (let ((user-input (read-line *query-io*)))
+ (if (string= user-input answer)
+ (progn
+ (format t "RIGHT!~%")
+ (return)) ; Exit the loop if the answer is correct
+ (format t "WRONG. Try again.~%")))))))
 (defun play-crossword (clues)
-  (format t "Welcome to the crossword puzzle!~%")
-  (format t "Let's get started.~%")
-  (print-crossword-grid *crossword-grid*)
-  (format t "~%CLUES:~%~%")
-  (loop for clue in clues
-    do (format t "Direction: [~a] ~a~%" (car clue) (caddr clue))
-    do (format t "Enter your answer: ")
-    do (finish-output)
-    do (let ((user-input (read-line *query-io*)))
-          (check-answer user-input (cadddr clue))))
-  (format t "Congratulations, you've completed the puzzle!~%")
-  (print-answer *crossword-grid*)
+ (format t "Welcome to the crossword puzzle!~%")
+ (format t "Let's get started.~%")
+ (print-crossword-grid *crossword-grid*)
+ (format t "~%CLUES:~%~%")
+ (loop for clue in clues
+ do (format t "Direction: [~a] ~a~%" (car clue) (caddr clue))
+ do (format t "Enter your answer: ")
+ do (finish-output)
+ do (let ((user-input (read-line *query-io*)))
+ (check-answer user-input (cadddr clue))))
+ (format t "Congratulations, you've completed the puzzle!~%")
+ (print-answer *crossword-grid*)
 )
-
 (play-crossword *clues*)
